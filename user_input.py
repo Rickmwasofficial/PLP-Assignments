e396ea653cdd734ea94eea69f60d7d5b5ce846c2
Rickmwasofficial <rickmwasofficial@gmail.com>
2024-02-22 15:08:15 +0300

    Added the first Python Assignment

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("Enter Your Name: ")
+age = input("Enter your Age: ")
+location = input("Enter Your Location: ")
+
+print(f"Hello {name}, you are {age} years old and you live in {location}.")
